3368a96ecff71b8103c47b811fae7eb03f84331a
Maximo Mercado <mr3003gean@gmail.com>
2024-10-28 01:48:41 -0500

    Se creo el navbar y footer para iniciar el proyecto, posiblemente con futuras mejoras

--- Culemania-React/src/components/Navbar.jsx
+++ Culemania-React/src/components/Navbar.jsx
@@ -1,20 +1,32 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import '../styles/components/Navbar.css';
-import FC_Barcelona from '../assets/img/FC_Barcelona.png';
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/components/Navbar.css";
+import FC_Barcelona from "../assets/img/FC_Barcelona.png";
 
 function Navbar() {
   return (
     <nav className="navbar">
       <h1 className="navbar-title">
-        <img src={FC_Barcelona} alt="FC Barcelona Logo" className="navbar-logo" />
+        <img
+          src={FC_Barcelona}
+          alt="FC Barcelona Logo"
+          className="navbar-logo"
+        />
         CULÉMANIA
       </h1>
       <ul className="navbar-links">
-        <li><Link to="/">INICIO</Link></li>
-        <li><Link to="/noticias">NOTICIAS</Link></li>
-        <li><Link to="/plantilla">PLANTILLA</Link></li>
-        <li><Link to="/historia">HISTORIA</Link></li>
+        <li>
+          <Link to="/">INICIO</Link>
+        </li>
+        <li>
+          <Link to="/noticias">NOTICIAS</Link>
+        </li>
+        <li>
+          <Link to="/plantilla">PLANTILLA</Link>
+        </li>
+        <li>
+          <Link to="/historia">HISTORIA</Link>
+        </li>
       </ul>
     </nav>
   );
