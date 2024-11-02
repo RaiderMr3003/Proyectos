a6e26884b448c939bd7f6fc91b102e990265498c
Maximo Mercado <mr3003gean@gmail.com>
2024-11-02 02:14:20 -0500

    aSe realizo la importación de la api correctamente para poder mostrar los partidos del barcelona, pendiente estilos para mejor vista

--- Culemania-React/src/components/Matches.jsx
+++ Culemania-React/src/components/Matches.jsx
@@ -1,66 +1,90 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/components/Matches.css";
 
-function Matches() {
+
+const Matches = () => {
+  const [matches, setMatches] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchMatches = async () => {
+      try {
+        const response = await fetch("http://localhost:3000/api/matches");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setMatches(data);
+      } catch (error) {
+        console.error("Error fetching matches:", error);
+        setError("Error al obtener los partidos");
+      }
+    };
+
+    fetchMatches();
+  }, []);
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
   return (
     <section className="matches-container">
-      <article className="match-card">
-        <header className="match-header">
-          <div className="match-tournament">
-            <img
-              src="https://assets.laliga.com/assets/logos/LL_RGB_h_color/LL_RGB_h_color.png"
-              alt="Tournament Logo"
-            />
-            <span>La Liga</span>
-          </div>
-          <div className="match-date">
-            <time className="match-date" dateTime="2023-08-12T19:00">
-              26 Oct <strong>21:00</strong>
-            </time>
-          </div>
-        </header>
-
-        <div className="match-content">
-          <div className="column">
-            <div className="team team--home">
-              <figure className="team-logo">
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/sco/thumb/5/56/Real_Madrid_CF.svg/640px-Real_Madrid_CF.svg.png"
-                  alt="Chelsea Logo"
-                />
-              </figure>
-              <h2 className="team-name">Real Madrid</h2>
+      {matches.map((match, index) => (
+        <article key={index} className="match-card">
+          <header className="match-header">
+            <div className="match-tournament">
+              <div
+                className="competition-logo"
+                dangerouslySetInnerHTML={{ __html: match.competicion.logo }}
+              />
             </div>
-          </div>
+            <div className="match-date">
+              <time className="match-date">
+                {match.fecha} <strong>{match.hora}</strong>
+              </time>
+            </div>
+          </header>
 
-          <div className="column">
-            <div className="match-details">
-              <div className="match-score">
-                <span className="match-score-number match-score-number--leading">
-                  0
-                </span>
-                <span className="match-score-divider">:</span>
-                <span className="match-score-number">4</span>
+          <div className="match-content">
+            <div className="column">
+              <div className="team team--home">
+                <figure className="team-logo">
+                  <img
+                    src={match.local.logo}
+                    alt={`${match.local.equipo} logo`}
+                  />
+                </figure>
+                <h2 className="team-name">{match.local.equipo}</h2>
+              </div>
+            </div>
+
+            <div className="column">
+              <div className="match-details">
+                <div className="match-score">
+                  <span className="match-score-number match-score-number--leading">
+                    VS
+                  </span>
+                </div>
+              </div>
+            </div>
+
+            <div className="column">
+              <div className="team team--away">
+                <figure className="team-logo">
+                  <img
+                    src={match.visitante.logo}
+                    alt={`${match.visitante.equipo} logo`}
+                  />
+                </figure>
+                <h2 className="team-name">{match.visitante.equipo}</h2>
               </div>
             </div>
           </div>
-
-          <div className="column">
-            <div className="team team--away">
-              <figure className="team-logo">
-                <img
-                  src="https://upload.wikimedia.org/wikipedia/en/thumb/4/47/FC_Barcelona_%28crest%29.svg/2020px-FC_Barcelona_%28crest%29.svg.png"
-                  alt="West Ham Logo"
-                />
-              </figure>
-              <h2 className="team-name">FC BARCELONA</h2>
-            </div>
-          </div>
-        </div>
-      </article>
-      
+        </article>
+      ))}
     </section>
   );
-}
+};
 
 export default Matches;
