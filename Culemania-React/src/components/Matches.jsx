d6de1917903d32b290ca2c75bc090805b97515b3
Maximo Mercado <mr3003gean@gmail.com>
2024-11-04 18:52:37 -0500

    Se instalo react-slick para realizar el carousel del calendario de partidos, ademas tambien se intento darles un buen diseño

--- Culemania-React/src/components/Matches.jsx
+++ Culemania-React/src/components/Matches.jsx
@@ -1,8 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
+import Slider from "react-slick";
 import "../styles/components/Matches.css";
-
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const Matches = () => {
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 5,
+    responsive: [
+      { breakpoint: 1800, settings: { slidesToShow: 3, slidesToScroll: 3,} },
+      { breakpoint: 1100, settings: { slidesToShow: 2, slidesToScroll: 2 } },
+      { breakpoint: 725, settings: { slidesToShow: 1, slidesToScroll: 1 } },
+    ],
+  };
+
   const [matches, setMatches] = useState([]);
   const [error, setError] = useState(null);
 
@@ -10,9 +25,7 @@ const Matches = () => {
     const fetchMatches = async () => {
       try {
         const response = await fetch("http://localhost:3000/api/matches");
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
+        if (!response.ok) throw new Error("Network response was not ok");
         const data = await response.json();
         setMatches(data);
       } catch (error) {
@@ -24,66 +37,44 @@ const Matches = () => {
     fetchMatches();
   }, []);
 
-  if (error) {
-    return <div>{error}</div>;
-  }
+  if (error) return <div>{error}</div>;
 
   return (
-    <section className="matches-container">
+    <Slider {...settings}>
       {matches.map((match, index) => (
-        <article key={index} className="match-card">
-          <header className="match-header">
-            <div className="match-tournament">
-              <div
-                className="competition-logo"
-                dangerouslySetInnerHTML={{ __html: match.competicion.logo }}
-              />
-            </div>
-            <div className="match-date">
-              <time className="match-date">
-                {match.fecha} <strong>{match.hora}</strong>
-              </time>
-            </div>
-          </header>
-
-          <div className="match-content">
-            <div className="column">
-              <div className="team team--home">
-                <figure className="team-logo">
-                  <img
-                    src={match.local.logo}
-                    alt={`${match.local.equipo} logo`}
-                  />
-                </figure>
-                <h2 className="team-name">{match.local.equipo}</h2>
+        <div key={index} className="match-card">
+          <div className="competition-header-container">
+            <div
+              className="competition-logo"
+              dangerouslySetInnerHTML={{ __html: match.competicion.logo }}
+            />
+            <div className="competition-header">{match.jornada}</div>
+          </div>
+          <div className="match-info">
+            <div className="teams">
+              <div className="team">
+                <img
+                  src={match.local.logo}
+                  alt={`${match.local.equipo} logo`}
+                />
+                <span>{match.local.equipo}</span>
+              </div>
+              <span className="versus">VS</span>
+              <div className="team">
+                <img
+                  src={match.visitante.logo}
+                  alt={`${match.visitante.equipo} logo`}
+                />
+                <span>{match.visitante.equipo}</span>
               </div>
             </div>
-
-            <div className="column">
-              <div className="match-details">
-                <div className="match-score">
-                  <span className="match-score-number match-score-number--leading">
-                    VS
-                  </span>
-                </div>
-              </div>
-            </div>
-
-            <div className="column">
-              <div className="team team--away">
-                <figure className="team-logo">
-                  <img
-                    src={match.visitante.logo}
-                    alt={`${match.visitante.equipo} logo`}
-                  />
-                </figure>
-                <h2 className="team-name">{match.visitante.equipo}</h2>
-              </div>
+            <div className="match-time">
+              <span>{match.hora}</span><span>{match.fecha}</span>
             </div>
           </div>
-        </article>
+        </div>
       ))}
-    </section>
+    </Slider>
   );
 };
 
